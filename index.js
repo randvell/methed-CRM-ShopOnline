ba22df67276e7e4e64a7f6ce5d4a94fc3bfcae89
Nikita Zimin <n.zimin@aristos.pw>
2023-11-25 00:03:02 +0300

    Module 4 Lesson 1

---
+++ index.js
@@ -0,0 +1,7 @@
+const qty = 3;
+const productName = 'Спортивные штаны Adidas';
+const productCategory = 'Брюки';
+const price = 1800;
+
+console.log('Товар: ' + productName);
+console.log('Общая стоимость: ' + price * qty);

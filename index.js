daf2ab6be1aa479c54e20400b48104c0e7f97ff0
Nikita Zimin <n.zimin@aristos.pw>
2023-12-05 22:46:20 +0300

    Module 4 Start

--- index.js
+++
@@ -1,7 +0,0 @@
-const qty = 3;
-const productName = 'Спортивные штаны Adidas';
-const productCategory = 'Брюки';
-const price = 1800;
-
-console.log('Товар: ' + productName);
-console.log('Общая стоимость: ' + price * qty);

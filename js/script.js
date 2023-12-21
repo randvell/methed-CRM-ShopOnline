95dc9e25fee4d48adba3db2f2cb538227accf0ed
Nikita Zimin <n.zimin@aristos.pw>
2023-12-21 12:29:07 +0300

    Module 5 Lesson 5

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ const form = productModal.querySelector('.form');
 const discountCheckbox = form.querySelector('#discount_checkbox');
 const discountValue = form.querySelector('#discount_value');
 const totalPrice = form.querySelector('.summary__value');
+
+const overlay = document.querySelector('.overlay');
+const showOverlay = () => {
+  overlay.classList.add('overlay--visible')
+}
+const hideOverlay = () => {
+  overlay.classList.remove('overlay--visible')
+}
+
+productModal.addEventListener('click', event => {
+  event.stopPropagation();
+});
+
+const addProductBtn = document.querySelector('.button--add-product');
+addProductBtn.addEventListener('click', showOverlay);
+overlay.addEventListener('click', hideOverlay);
+formCloseBtn.addEventListener('click', hideOverlay);

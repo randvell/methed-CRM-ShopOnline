5513662a0dd5db7b4dd06f8bc3785c609faec922
Nikita Zimin <n.zimin@aristos.pw>
2024-02-17 20:59:08 +0300

    Goods page size

--- js/api.js
+++ js/api.js
@@ -1,14 +1,18 @@
 const API_URL = 'https://button-hypnotic-pixie.glitch.me/';
 
-export const getGoods = async () => {
-  const response = await fetch(`${API_URL}/api/goods`);
+export const getGoods = async (size = 'all') => {
+  const response = await fetch(`${API_URL}/api/goods?size=` + size);
   if (!response.ok) {
     throw new Error(`Ошибка сервера: ${response.statusText}`);
   }
 
   const result = await response.json();
-  if (!result.goods) {
+  if (!result || (size !== 'all' && !result.goods)) {
     throw new Error(`В ответе сервера не обнаружены товары`);
+  }
+
+  if (size === 'all') {
+    return { goods: result };
   }
 
   return result;
